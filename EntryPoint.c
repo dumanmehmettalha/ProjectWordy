7d623aa5923938c3c15e13d4030150971b67af77
Mehmet Kocamaz <kocamazmehmet@hotmail.com>
2023-08-23 02:29:01 +0300

    Bugs fixed #1
    Some infinite loops and bugs fixed. Redundant code removed and edited.

--- EntryPoint.c
+++ EntryPoint.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "Functions.h"
+//#include <locale.h>
 
-// The Project Wordy
 
 int main() {
 	Execute();
--- Functions.h
+++ Functions.h
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//#include <locale.h>
 
-void ExecuteAddWord() {
+void AddWord() {
+
 	FILE* filePointer = NULL; // Dosya structýndan pointer oluþturuyoruz ve initialize edioruz. 
 	fopen_s(&filePointer, "Wordy.txt", "a+"); // fopen fonksiyonuna bu pointerýn adresini, dosya adýný ve tipini yolluyoruz.
 	
 	if (filePointer == NULL) {	// Memoryde boþ yer olmadýðý bir anda dosya açýlamayabilir. Ve pointer hata olarak NULL döndürür. Kullanýcýya bunu söylemek ve bazý fonksiyonlarýn kullanýmýnda hata almamak için bu condition kontrol edilir.
-		puts("File could not be opened./\\Dosya acilamadi.");
+		puts("\nFile could not be opened./\\Dosya acilamadi.");
 	}
 	else { // Dosya açýldý 
 		char newWordEnglish[15];
 		char newWordTurkish[15];
-		printf("%s", "Enter the english word first and then the turkish translate of it.\n:");
-
+		printf("\n%s", "Enter the english word first and then the turkish translate of it.\n");
 		scanf_s("%s %s", newWordEnglish, _countof(newWordEnglish), newWordTurkish, _countof(newWordTurkish)); // scanf_s hatasýz kullanýmý
+		
 
+		// BUNUNLA ÝLGÝLENMEM GEREKLÝ ! 
+		/*gets_s(newWordEnglish, sizeof(newWordEnglish));
 
-		fprintf(filePointer, "%s --> %s\n", newWordEnglish, newWordTurkish);
+		int c = 0;
+		while ((c = getchar()) != "\n" && c != EOF) { break; }
+
+		printf("Turkish Word:");
+		gets_s(newWordTurkish, 15);
+		
+		c = 0;
+		while ((c = getchar()) != "\n" && c != EOF) { break; }*/
+
+		fprintf(filePointer, "%s-%s", newWordEnglish, newWordTurkish);
+		fprintf(filePointer, "\n");
 		fclose(filePointer);
 	}
-	
-
 }
 
-void deleteData(char word[15]) {
+void DeleteData(char word[15]) {
+
 	FILE* fp;  //r+ modunda dosya açýldý çünkü baþtan sona bakýlýp silinmek istenen kelime bulunacak ve üzerine boþluk yazýlacak.
 	char temp[15];
 	long int pos;
 	errno_t err = fopen_s(&fp, "Wordy.txt", "r+");
 	if (err != 0) {
-		printf("File could not be opened.");
+		printf("%s", "\nFile could not be opened.");
 		return;
 	}
 
@@ -42,29 +55,26 @@ void deleteData(char word[15]) {
 			//printf("%ld", pos);
 			fseek(fp, pos, SEEK_SET); //imleci dosyanýn baþýndan pos kadar ilerletir.
 			fprintf(fp, "%*s", strlen(temp), "");
-			printf_s("The word you have entered have just deleted.\n");
+			printf_s("\nThe word you have entered have just deleted.\n");
 			break;
 		}
-		else {
-			printf_s("The word you have entered is not in the text file.\n");
-		}
+	}
+	if (strcmp(temp, word) != 0) 
+	{
+		printf_s("\nThe word you have entered is not in the text file.\n");
 	}
 
 	fclose(fp);
 }
 
-void ExecuteDeleteData() {
-		char word[15];
-		printf("Enter a word you want to delete:  ");
-		scanf_s("%s", word, sizeof(word));
-		deleteData(word);
-}
+void ShowFile() {
 
-void ExecuteShowFile() {
 	FILE* fp;
+
+
 	errno_t err = fopen_s(&fp, "Wordy.txt", "r"); // Okunacak dosyanýn adýný doðru þekilde belirtin
 	if (err != 0) {
-		printf("File could not be opened.");
+		printf("%s", "\nFile could not be opened.");
 		return 1; // Programý hata kodu ile sonlandýr
 	}
 
@@ -72,17 +82,16 @@ void ExecuteShowFile() {
 	while (fgets(line, sizeof(line), fp) != NULL) {
 		printf("%s", line);
 	}
-
 	fclose(fp);
 }
 
 void Instructions(int* userInput)
-{
-
-	printf("%s", "If you want to enter a word to program press 1,\nBir kelime eklemek istiyorsaniz 1 giriniz,\n\n");
-	printf("%s", "If you want to delete a word from program press 2,\nBir kelime silmek istiyorsaniz 2 giriniz,\n\n");
-	printf("%s", "If you want to see all of the words press 3,\nProgramdaki tum kelimeleri gormek istiyorsaniz 3 giriniz,\n\n");
-	printf("%s", "If you want to terminate the program press 4\nProgrami sonlandirmak istiyorsaniz 4 giriniz,\n\nChoice/Secim: ");
+{	
+//	setlocale(LC_ALL, ".utf8");
+	printf("\n%s", "If you want to enter a word to program press 1,\nBir kelime eklemek istiyorsanýz 1 giriniz,\n\n");
+	printf("%s", "If you want to delete a word from program press 2,\nBir kelime silmek istiyorsanýz 2 giriniz,\n\n");
+	printf("%s", "If you want to see all of the words press 3,\nProgramdaki tüm kelimeleri görmek istiyorsanýz 3 giriniz,\n\n");
+	printf("%s", "If you want to terminate the program press 4\nProgramý sonlandýrmak istiyorsanýz 4 giriniz,\n\nChoice/Seçim: ");
 	scanf_s("%d", userInput);
 }
 
@@ -90,27 +99,34 @@ void Instructions(int* userInput)
 
 void Execute() {
 	int userInput = 0;
+	char word[15];
 	while (4 != userInput)
 	{
 		Instructions(&userInput);
 		switch (userInput)
 		{
 		case 1:
-			ExecuteAddWord();
+			AddWord();
+			userInput = 0;
 			break;
 		case 2:
-			ExecuteDeleteData();
+			printf("%s", "\nEnter a word you want to delete:  ");
+			scanf_s("%s", word, sizeof(word));
+			DeleteData(word);
+			userInput = 0;
 			break;
 		case 3:
 			puts("");
-			ExecuteShowFile();
+			ShowFile();
 			puts("");
+			userInput = 0;
 			break;
 		case 4:
 			printf("%s", "\nSee you again./Gorusuruz.\n");
 			break;
 		default:
-			printf("%s", "Invalid Choise/Hatali Secim");
+			printf("%s", "\nInvalid Choise!/\\Hatali Secim!\n");
+			userInput = 4;
 			break;
 		}
 	}
